3267d3e0c0c598d7d617c974a33d9ceb98fe5556
David Rosillo <drr538@gmail.com>
2018-07-03 11:53:45 -0500

    Initial commit and initial development

---
+++ fitness_frog/fitness_frog/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace fitness_frog
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Prompt the user for minutes excercised
+            Console.Write("Enter how many minutes you excercised: ");
+
+            string entry = Console.ReadLine();
+
+            // Add minutes excercised to total
+            // Display total minutes excercised to the screen
+            // Repeat until the user quits
+        }
+    }
+}
