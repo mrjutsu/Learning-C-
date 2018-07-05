e66ecd2e8508ccf2e60ad97439d182913f1f5dde
David Rosillo <drr538@gmail.com>
2018-07-05 17:06:26 -0500

    Encapsulation

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -26,7 +26,26 @@ namespace Game
 
             try
             {
-                MapLocation mapLocation = new MapLocation(20, 20, map);
+
+                Path path = new Path(
+                    new[] {
+                        new MapLocation(0,2,map),
+                        new MapLocation(1,2,map),
+                        new MapLocation(2,2,map),
+                        new MapLocation(3,2,map),
+                        new MapLocation(4,2,map),
+                        new MapLocation(5,2,map),
+                        new MapLocation(6,2,map),
+                        new MapLocation(7,2,map)
+                    }
+                );
+
+                // MapLocation mapLocation = new MapLocation(20, 20, map);
+                MapLocation location = path.GetLocationAt(0);
+                if (location != null)
+                {
+                    Console.WriteLine(location.X + "," + location.Y);
+                }
             }
             catch (OutOfBoundsException ex)
             {
