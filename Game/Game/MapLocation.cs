47288f842c7de20be838c6e9bdaf77cdcfb37e1a
David Rosillo <drr538@gmail.com>
2018-07-06 15:26:45 -0500

    Loops

--- Game/Game/MapLocation.cs
+++ Game/Game/MapLocation.cs
@@ -11,5 +11,10 @@ namespace Game
                 throw new OutOfBoundsException(x + "," + y + " is outside the boundaries of the map.");
             }
         }
+
+        public bool InRangeOf(MapLocation location, int range)
+        {
+            return DistanceTo(location) <= range;
+        }
     }
 }
--- Game/Game/Tower.cs
+++ Game/Game/Tower.cs
@@ -3,8 +3,40 @@ namespace Game
 {
     public class Tower
     {
-        public Tower()
+
+        private readonly MapLocation _location;
+
+        public Tower(MapLocation location)
         {
+            _location = location;
+        }
+
+        public void FireOnInvaders(Invader[] invaders)
+        {
+            // int index = 0;
+            /*
+            while (index < invaders.Length){
+                Invader invader = invaders[index];
+
+                index++;
+            }
+            */
+
+            /*
+            for (index = 0; index < invaders.Length; index++)
+            {
+                Invader invader = invaders[index];
+            }
+            */
+
+            foreach (Invader invader in invaders)
+            {
+                if (invader.IsActive && _location.InRangeOf(invader.Location, 1))
+                {
+                    invader.DecreaseHealth(1);
+                    break;
+                }
+            }
         }
     }
 }
