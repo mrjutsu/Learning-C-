2859736615c91405f8bd3dfbb147e2b8ca30b95e
David Rosillo <drr538@gmail.com>
2018-07-10 11:07:01 -0500

    Loaded CSV file

---
+++ SoccerStats/SoccerStats/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace SoccerStats
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            //var files = directory.GetFiles("*.txt");
+            //foreach (var file in files)
+            //{
+            //    Console.WriteLine(file.Name);
+            //}
+
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadFile(fileName);
+            Console.WriteLine(fileContents);
+            //var file = new FileInfo(filename);
+            //if (file.Exists)
+            //{
+            //    //File will be closed when this reaches the end
+            //    using (var reader = new StreamReader(file.FullName))
+            //    {
+            //        Console.SetIn(reader);
+            //        Console.WriteLine(Console.ReadLine());
+            //    }
+            //    Console.ReadLine();
+            //}
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
