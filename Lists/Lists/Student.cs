d6aeaafe459314292ed012f0fc83c3016e50f285
David Rosillo <drr538@gmail.com>
2018-07-18 11:51:45 -0500

    Dictionary

--- Lists/Lists/Student.cs
+++ Lists/Lists/Student.cs
@@ -27,5 +27,17 @@ namespace Lists
         {
             return Name.GetHashCode() + GradeLevel.GetHashCode();
         }
+
+        public override bool Equals(object obj)
+        {
+            Student that = obj as Student;
+
+            if (that == null)
+            {
+                return false;
+            }
+
+            return this.Name == that.Name && this.GradeLevel == that.GradeLevel;
+        }
     }
 }
---
+++ MorseTranslator/MorseTranslator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MorseTranslator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write(": ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+
+                string output = MorseCodeTranslator.ToMorse(input);
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
